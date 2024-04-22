7611bd640890c5b83c7074539eae18ff0330e92a
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-04-22 22:31:48 +0000

    dsa

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const $navbar = document.querySelector("[data-navbar]");
-const $navToggler = document.querySelector("[data-nav-toggler]");
+// navbar toggle in mobile
 
-if ($navbar && $navToggler) {
-    $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
-} else {
-    console.error("Navbar or NavToggler element not found.");
-}
+const $navbar = document.querySelector("[data-navbar]")
+const $navToggler = document.querySelector("[data-nav-toggler]")
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"))
