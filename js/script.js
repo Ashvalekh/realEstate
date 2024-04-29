1309502c622f4a6c108ad57dad58eca5f3f4002a
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-04-29 10:06:18 +0000

    adsd

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-'use strict';
+"use strict";
 
 // navbar toggle in mobile
 
-const $navbar = document.querySelector("[data-navbar]")
-const $navToggler = document.querySelector("[data-nav-toggler]")
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
 
-$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"))
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
 
 // Header scroll state
 
 const $header = document.querySelector("[data-header]");
 
-window.addEventListener("scroll", e => {
-    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+window.addEventListener("scroll", (e) => {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Add to favorite button toggle
+ */
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach(($toggleBtn) => {
+  $toggleBtn.addEventListener("click", () => {
+    $toggleBtn.classList.toggle("active");
+  });
 });
